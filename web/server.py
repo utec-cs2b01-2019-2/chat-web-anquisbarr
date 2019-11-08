9136d0c278cef16be98406caf63d7d03bce3e0b5
jbellidoutec <36900746+jbellidoutec@users.noreply.github.com>
2019-11-08 09:57:03 -0500

    Update server.py

--- web/server.py
+++ web/server.py
@@ -306,4 +306,4 @@ def delete_group(id):
 
 if __name__ == '__main__':
     app.secret_key = ".."
-    app.run()#(debug=True,port=80, threaded=True, use_reloader = False)
+    app.run(debug=True,port=80, threaded=True, use_reloader = False)
