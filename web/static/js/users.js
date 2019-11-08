6f71e7b39a5f8633f9a149ee877d980a3d67087a
Sebastian Q.B <sebastian.quispe.b@utec.edu.pe>
2019-11-08 10:20:10 -0500

    Fix?

--- web/static/js/users.js
+++ web/static/js/users.js
@@ -1,5 +1,5 @@
 $(function(){
-    var url = 'http:://'+document.domain+':'+location.port +'/users';
+    var url = "http:127.0.0.1:8000/users";
 
     $("#grid").dxDataGrid({
         dataSource: DevExpress.data.AspNet.createStore({
